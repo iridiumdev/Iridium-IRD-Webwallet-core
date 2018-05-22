e2e6e7b2542c4738b6302517166c6d2048333ea7
Daniel Clasen <mail@danielclasen.de>
2018-05-22 13:13:20 +0200

    pull docker images during bean bootstrap phase

--- server/src/main/java/cash/ird/webwallet/server/config/DockerClientConfig.java
+++ server/src/main/java/cash/ird/webwallet/server/config/DockerClientConfig.java
@@ -3,10 +3,13 @@ package cash.ird.webwallet.server.config;
 import cash.ird.webwallet.server.config.props.DockerProperties;
 import com.spotify.docker.client.DefaultDockerClient;
 import com.spotify.docker.client.DockerClient;
+import com.spotify.docker.client.exceptions.DockerException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class DockerClientConfig {
 
@@ -14,6 +17,15 @@ public class DockerClientConfig {
     @Autowired
     public DockerClient dockerClient(DockerProperties dockerProperties) {
         final DockerClient docker = new DefaultDockerClient(dockerProperties.getSocket());
+
+        dockerProperties.getPullImages().forEach(image -> {
+            try {
+                docker.pull(image);
+            } catch (DockerException | InterruptedException e) {
+                log.error("Could not pull image {}! Exception is: ", image, e);
+            }
+        });
+
         return docker;
     }
 
--- server/src/main/java/cash/ird/webwallet/server/config/props/DockerProperties.java
+++ server/src/main/java/cash/ird/webwallet/server/config/props/DockerProperties.java
@@ -4,11 +4,16 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "docker")
 public class DockerProperties {
 
     private String socket = "unix:///var/run/docker.sock";
+
+    private List<String> pullImages = new ArrayList<>();
 
 }
