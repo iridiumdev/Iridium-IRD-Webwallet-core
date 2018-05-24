04aa6ceb1fc7ac0e4099033c789b0b85cd301439
Daniel Clasen <mail@danielclasen.de>
2018-05-25 00:02:30 +0200

    limit swagger docs to relevant stuff only

--- server/src/main/java/cash/ird/webwallet/server/config/SwaggerConfig.java
+++ server/src/main/java/cash/ird/webwallet/server/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import cash.ird.webwallet.server.rest.CurrentUser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -13,6 +14,9 @@ public class SwaggerConfig {
     @Bean
     public Docket docket(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("cash.ird.webwallet.server.rest.controller"))
+                .build()
                 .ignoredParameterTypes(AuthenticationPrincipal.class)
                 .ignoredParameterTypes(CurrentUser.class);
     }
